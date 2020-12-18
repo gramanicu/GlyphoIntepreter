025e5ee30bb23df56ab4c743f623c0811995763a
Grama Nicolae <gramanicu@gmail.com>
2020-12-18 15:53:05 +0200

    Added the stack object

--- src/Glypho/Stack.cpp
+++ src/Glypho/Stack.cpp
@@ -7,4 +7,115 @@
 
 #include "Stack.hpp"
 
-using namespace Glypho::Core;
+namespace Glypho::Core { 
+        Stack::Stack() {
+            data = std::list<Integer>();
+        }
+
+        void Stack::Push() {
+            data.push_front(Integer("1"));
+        }
+
+        void Stack::Pop() {
+            Helpers::MUST_NOT(data.size() == 0, Throwable::message(Throwable::RuntimeException::EMPTY_STACK) + "\n");
+
+            data.pop_front();
+        }
+
+        Integer Stack::Peek() const {
+            Helpers::MUST_NOT(data.size() == 0, Throwable::message(Throwable::RuntimeException::EMPTY_STACK) + "\n");
+
+            return data.front();
+        }
+
+        void Stack::Input(const Integer& value) {
+            data.push_front(value);
+        }
+
+        Integer Stack::Output() {
+            Helpers::MUST_NOT(data.size() == 0, Throwable::message(Throwable::RuntimeException::EMPTY_STACK) + "\n");
+
+            Integer value = data.front();
+            data.pop_front();
+            return value;
+        }
+
+        void Stack::Dup() {
+            Helpers::MUST_NOT(data.size() == 0, Throwable::message(Throwable::RuntimeException::EMPTY_STACK) + "\n");
+
+            Integer value = data.front();
+            data.push_front(value);
+        }
+
+        void Stack::Swap() {
+            Helpers::MUST(data.size() >= 2, Throwable::message(Throwable::RuntimeException::INSUFFICIENT_STACK_SIZE) + "\n");
+
+            Integer value1 = data.front();
+            data.pop_front();
+            Integer value2 = data.front();
+            data.pop_front();
+
+            data.push_front(value1);
+            data.push_front(value2);
+        }
+
+        void Stack::Rotate() {
+            Helpers::MUST_NOT(data.size() == 0, Throwable::message(Throwable::RuntimeException::EMPTY_STACK) + "\n");
+
+            Integer value = data.front();
+            data.pop_front();
+            data.push_back(value);
+        }
+
+        void Stack::ReverseRotate() {
+            Helpers::MUST_NOT(data.size() == 0, Throwable::message(Throwable::RuntimeException::EMPTY_STACK) + "\n");
+
+            Integer value = data.back();
+            data.pop_back();
+            data.push_front(value);
+        }
+
+        void Stack::Add() {
+            Helpers::MUST(data.size() >= 2, Throwable::message(Throwable::RuntimeException::INSUFFICIENT_STACK_SIZE) + "\n");
+
+            Integer value1 = data.front();
+            data.pop_front();
+            Integer value2 = data.front();
+            data.pop_front();
+
+            data.push_back(value1 + value2);
+        }
+
+        void Stack::Multiply() {
+            Helpers::MUST(data.size() >= 2, Throwable::message(Throwable::RuntimeException::INSUFFICIENT_STACK_SIZE) + "\n");
+
+            Integer value1 = data.front();
+            data.pop_front();
+            Integer value2 = data.front();
+            data.pop_front();
+
+            data.push_back(value1 * value2);
+        }
+
+        void Stack::Negate() {
+            Helpers::MUST_NOT(data.size() == 0, Throwable::message(Throwable::RuntimeException::EMPTY_STACK) + "\n");
+
+            Integer value = data.front();
+            value = Integer("0") - value;
+
+            data.pop_front();
+            data.push_front(value);
+        }
+
+        std::vector<Integer> Stack::Out_K_Elems(const uint64_t count) {
+            Helpers::MUST(data.size() >= count, Throwable::message(Throwable::RuntimeException::INSUFFICIENT_STACK_SIZE) + "\n");
+
+            std::vector<Integer> values;
+            for(uint64_t i = 0; i < count; ++i) {
+                values.push_back(data.front());
+                data.pop_front();
+            }
+
+            return values;
+        }
+}
--- src/Glypho/Stack.hpp
+++ src/Glypho/Stack.hpp
@@ -7,7 +7,7 @@
 #pragma once
 
 #include <iostream>
-#include <stack>
+#include <list>
 #include <string>
 #include <vector>
 
@@ -16,6 +16,100 @@
 
 namespace Glypho::Core {
     class Stack {
-        std::stack<Integer> data;
+        private:
+        std::list<Integer> data;
+
+        public:
+        /**
+         * @brief Construct a new Stack object
+         * 
+         */
+        Stack();
+        
+        // Basic Stack Operations
+        /**
+         * @brief Add an element at the top of the stack with the value of 1
+         * 
+         */
+        void Push();
+
+        /**
+         * @brief Take the element from the top of the stack
+         * 
+         * @return Integer 
+         */
+        void Pop();
+
+        /**
+         * @brief Get the element from the top of the stack, but don't remove it
+         * 
+         * @return Integer The value
+         */
+        Integer Peek() const;
+
+        /**
+         * @brief Add an element at the top of the stack with the specified value
+         * 
+         * @param value New value
+         */
+        void Input(const Integer& value);
+
+        /**
+         * @brief Get the element at the top of the stack and remove it
+         * 
+         * @return Integer The top value
+         */
+        Integer Output();
+
+        // Complex Stack Operations
+        /**
+         * @brief Duplicate the element at the top of the stack
+         * 
+         */
+        void Dup();
+
+        /**
+         * @brief Swaps the top 2 elements in the stack
+         * 
+         */
+        void Swap();
+        
+        /**
+         * @brief Put the top element at the back
+         * 
+         */
+        void Rotate();
+
+        /**
+         * @brief Put the back element at the top
+         * 
+         */
+        void ReverseRotate();
+
+        /**
+         * @brief Takes the top two elements, computes their sum, and pushes the new element
+         * Will remove the two elements
+         */
+        void Add();
+
+        /**
+         * @brief Takes the top two elements, computes their product, and pushes the new element
+         * Will remove the two elements
+         */
+        void Multiply();
+
+        /**
+         * @brief Removes the top element and inserts back the inverse of that value
+         * 
+         */
+        void Negate();
+
+        /**
+         * @brief Removes and retuns the specified amount of elements from the stack
+         * 
+         * @param count The number of elements
+         * @return std::vector<Integer> An array of elements
+         */
+        std::vector<Integer> Out_K_Elems(const uint64_t count);
     };
 }    // namespace Glypho::Core
